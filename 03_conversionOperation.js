5ad67790246fb0ac8f20fdda266895ba6241222c
vickyvish31 <vicckyvish31@gmail.com>
2026-05-30 17:01:45 +0530

    Add 03_conversionOperation.js: Conversions and operations examples

---
+++ 03_conversionOperation.js
@@ -0,0 +1,68 @@
+let score = "33abc"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+// true => 1; false => 0
+// null => 0
+// undefined => NaN
+
+let isLoggedIn = "vicky"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "vicky" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2 + 2);
+//console.log(2 - 2);
+//console.log(2 * 2);
+//console.log(2 ** 3);
+//console.log(2 / 3);
+//console.log(2 % 3);
+
+let str1 = "hello"
+let str2 = " vicky"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log((3 + 4) * 5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
