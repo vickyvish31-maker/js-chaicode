fb292ff7605d96a9556a6fbfa64c26c047b47e61
vickyvish31 <vicckyvish31@gmail.com>
2026-05-30 13:33:17 +0530

    Initial commit: Add Javascript practice files

---
+++ 01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 1234
+var emailid = "vickyvish@gmail.com"
+let password = "12343"
+accountCity = "jaipur"
+let accountState;
+
+
+//accountId =3454 // not allowed
+emailid = "vicksgd@gmail.com"
+password = "66666"
+accountCity = "delhi"
+console.table([accountId, emailid, password, accountCity, accountState])
+
+/* prefer to not use var 
+     use let due to block scope */
---
+++ test.js
@@ -0,0 +1,8 @@
+//console.log("vicky")
+//console.log("gfvgd")
+let score = 33
+console.log(typeof score);
+//"33" => 33
+//"33abc" => NaN
+let stringNumber = String(score);
+console.log(typeof stringNumber);
