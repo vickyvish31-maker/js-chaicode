8bf3fff97fee630ee6a2676aed5cb49004c29f4d
vickyvish31 <vicckyvish31@gmail.com>
2026-05-30 16:32:47 +0530

    Update: Add datatypes and table output in 02_datatypes.js

--- 02_datatypes.js
+++ 02_datatypes.js
@@ -1,1 +1,12 @@
-console.log("hd")
+"use strict";//use newer version of js
+console.log("hd");
+//alert(3 + 3)//using node js not browser
+
+let name = "vicky"
+let age = 18
+let isloggedin = false
+let state = null
+console.table([name, age, isloggedin, state])
+console.log(typeof (age))
+console.log(typeof (null))
+//symbol =>used for uniquness
